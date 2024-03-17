431d7668d31379d991e8087937154f30c1f1c9f3
Evelyn Oliveira <evelynoliveira.tds1.senai@gmail.com>
2024-03-17 16:56:25 -0300

    Método 9: Array.prototype.flat()

---
+++ Metodo9.js
@@ -0,0 +1,14 @@
+//Método 9: Array.prototype.flat()
+//Esse método cria um novo array com todos os elementos de um array sbdimencionado concatenando em uma única dimensão
+
+//Exemplo 1: Juntando um array de arrays
+
+let letras = [["a", "b"], ["c", "d"], ["e", "f"]];
+
+console.log(letras.flat());
+
+//Exemplo 2: Juntando e removendo elementos indefinidos
+
+let palavras = [["abacaxi", "abacate"], , "acerola", "manga", , "goiaba "];
+
+console.log(palavras.flat())
