44676dfdfb5578b111b5fa351c52846d016b421c
Evelyn Oliveira <evelynoliveira.tds1.senai@gmail.com>
2024-03-17 20:33:51 -0300

    Metodo 2: Array.prototype.concat(). Correção de pontuação nas linhas 2, 4 e 11

--- Metodo2.js
+++ Metodo2.js
@@ -1,14 +1,14 @@
 //Método 2: Array.prototype.concat()
-//Esse array é utilizado para (mesclar/combinar) dois ou mais arrays
+//Esse array é utilizado para (mesclar/combinar) dois ou mais arrays.
 
-//Exemplo 1: Concatenamento dois arrays
+//Exemplo 1: Concatenamento dois arrays.
 
 let animais = ["Abelha", "Borboleta", "Cachorro"];
 let animais2 = ["Doninha", "Elefante"];
 
 console.log(animais.concat(animais2));
 
-//Exemplo 2: Concatenamento valores individuais 
+//Exemplo 2: Concatenamento valores individuais.
 
 let numeros = ["1", "2", "3", "4", "5"];
 let complementos = ["6"];
