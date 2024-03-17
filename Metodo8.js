047c533295ec372decd3dd30bf0b29023f076569
Evelyn Oliveira <evelynoliveira.tds1.senai@gmail.com>
2024-03-17 16:29:02 -0300

    Método 8: Array.prototype.findLast()

---
+++ Metodo8.js
@@ -0,0 +1,17 @@
+//Método 8: Array.prototype.findLast()
+//Esse 
+
+//Exemplo 1: Encontrar o índice do ultimo número par em um array
+
+let numeros = ["1", "3", "5", "6", "9", "11", "21"];
+let index = numeros.findLastIndex(num => num % 2 == 0);
+
+console.log(index);
+
+//Exemplo 2: Encontrar o índice do último nome que começa com a letra "Y"
+
+let nomes = ["Sophia", "Mel", "Amora", "Samantha", "Bob", "Yasmim", "Yara"];
+let lastIndex = nomes.findLastIndex(nome => nome.startsWith("Y"));
+
+console.log(lastIndex);
+
